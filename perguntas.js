6bd291fcef8e087064c492a7d5c76b4bd86a5e4f
rafaelakrone <161483510+rafaelakrone@users.noreply.github.com>
2024-09-25 13:46:45 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'historia',
+    'quem foi o primeiro presidente dos EUA ?',
+    'George Washington'
 )
 
 criaCartao(
@@ -12,12 +12,45 @@ criaCartao(
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Geografia ?',
+    'O planeta mais proximo so Sol e Mercurio'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Biologia',
+    'O que e fotossintese ?',
+    'Fotossintese e o processo pelo qual as plantas produzem energia a partir da luz solar'
+) 
+criaCartao(
+    'historia',
+    'Em que ano ocorreu a Revolucao Francesa ?',
+    'Ocorreu em 1789'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o rio mais longo do mundo?',
+    'Rio Nilo'
+
+)
+criaCartao(
+    'Curiosidade',
+    'Qual o alimento mais consumido no mundo ?',
+    'Arroz'
+)
+criaCartao(
+    'Ciencias',
+    'O que e a camada de ozonio?',
+    'uma regiao da estratosfera que protege a Terra dos raios ultravioleta do Sol'
+
+)
+criaCartao(
+    'Curiosidade',
+    'Qual o unico mamifero capaz de voar ?',
+    'Morcego'
+)
+criaCartao(
+    'Ciencias',
+    'O que sao as celulas tronco?',
+    'sao celulas que tem a capacidade de se transformar em varioa tipos de celulas do corpo '
 )
